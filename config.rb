cf83115f15d16cad1126b35501897ea71e7e9152
Ryan S. Rich <rrichrs@gmail.com>
2014-12-19 09:31:55 -0800

    Fixed three column layout and table styles

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ set :fonts_dir, 'assets/fonts'
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :smartypants => true, :disable_indented_code_blocks => true, :prettify => true, :tables => true, :with_toc_data => true, :no_intra_emphasis => true
 
-activate :syntax, :line_numbers => true
+activate :syntax
 
 activate :relative_assets
 set :relative_links, true
@@ -17,14 +17,4 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
-end
-
-activate :deploy do |deploy|
-  deploy.build_before = true
-  deploy.method   = :sftp
-  deploy.host     = 'wiki.catalyze.io'
-  deploy.port     = 22
-  deploy.path     = '/opt/catalyze_wiki'
-  deploy.user     = 'wikiuser'
-  deploy.password = 'sku(!8asdf_)1FF'
 end
