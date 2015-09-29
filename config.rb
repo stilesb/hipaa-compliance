94c52232c3a562e54a8285bb2c28d029785165bd
rr1000 <rrichrs@gmail.com>
2015-09-29 17:06:46 -0500

    So much

--- config.rb
+++ config.rb
@@ -20,38 +20,12 @@ set :markdown, :fenced_code_blocks => true, :smartypants => true, :tables => tru
 
 activate :syntax, :wrap => true
 
-set :url_root, 'https://engineering.catalyze.io'
+set :url_root, 'https://hipaa.catalyze.io'
 
 activate :search_engine_sitemap
 
 page "/sitemap.xml", :layout => false
 
-activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
-
-  blog.permalink = "{title}.html"
-  # Matcher for blog source files
-  blog.sources = "{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
-  blog.layout = "layout_blog"
-  # blog.summary_separator = /(READMORE)/
-  blog.summary_length = 500
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
-
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
-
-  # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
-end
-
-# Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
 #     "Helping"
